cb70fd3c58ca9ccef2f4b9843b59c62cc8c664ce
abrahamicm <32270874+abrahamicm@users.noreply.github.com>
2017-09-29 00:49:58 -0400

    Create selecciona.js
    codigo para manipular el dom

---
+++ selecciona.js
@@ -0,0 +1,5 @@
+$(".cheatsheet td:nth-of-type(1), .composite td:nth-of-type(1)").text(function(){this.innerHTML ="Metodo, "+this.innerHTML})
+$(".cheatsheet td:nth-of-type(2), .composite td:nth-of-type(2)").text(function(){this.innerHTML ="sentencia javascript "+this.innerHTML})
+$(".cheatsheet td:nth-of-type(3), .composite td:nth-of-type(3)").text(function(){this.innerHTML ="descripcion, "+this.innerHTML})
+
+

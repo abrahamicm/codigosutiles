e7289479c2f5c3fb1558a543e3a7aea1b5f3303a
abrahamicm <32270874+abrahamicm@users.noreply.github.com>
2017-10-15 19:04:38 -0400

    Create descargatext.js

---
+++ descargatext.js
@@ -0,0 +1,20 @@
+
+var saveData = (function () {
+    var a = document.createElement("a");
+    document.body.appendChild(a);
+    a.style = "display: none";
+    return function (data, fileName) {
+       // var json = JSON.stringify(data),
+            blob = new Blob([data], {type: "text/plain"}),
+            url = window.URL.createObjectURL(blob);
+        a.href = url;
+        a.download = fileName;
+        a.click();
+        window.URL.revokeObjectURL(url);
+    };
+}());
+
+var data = "hola mundooo";
+    fileName = "my-download.json";
+
+saveData(data, fileName);

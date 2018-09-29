b2dd723206f90e0819dfab074515988b8150037d
abrahamicm <32270874+abrahamicm@users.noreply.github.com>
2018-09-29 00:13:52 -0400

    Update creamenu.js

--- creamenu.js
+++ creamenu.js
@@ -1,30 +1,30 @@
-// crea un menu dinamico
 document.body.innerHTML="";
-menu = ["menu1",1,2,["memu2",1,2],["menu3",1,2]];
+menu = ["menu1",1,2,["memu2",3,4],["menu3",5,6]];
 
 function creaMenu(menu)
 {
-	document.write("<ul>")
+
 	for (x in menu) 
 	{
-		if (Array.isArray(menu[x]))
-	 	{
-	 		document.write("<li>"+menu[x][0]+"</li>");
-	 		
+			if (Array.isArray(menu[x]))
+		 	{
+		 		document.write("<li>")
+		 		document.write("<ul>");
+		 		creaMenu(menu[x]);
+		 		document.write("</ul>")
+		 		document.write("</li>")
+		 	}
 
-	 		creaMenu(menu[x]);
-	 	}
-
-	 	else 
-	 	{
-	 		document.write("<li>");
-	 		document.write(menu[x]);
-	 		document.write("</li>")
-	 	}
+		 	else 
+		 	{
+		 		document.write("<li>");
+		 		document.write(menu[x]);
+		 		document.write("</li>")
+		 	}
 	 	
 	 }
 
 
-	document.write("</ul>");
+
 }
 creaMenu(menu);
